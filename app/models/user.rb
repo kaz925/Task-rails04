11d8b6fe2b132b0c311c94825cfb9b914aa5428f
EC2 Default User <nisi_kazu_of_world@yahoo.co.jp>
2020-11-21 14:47:33 +0000

    課題再提出

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,10 @@
 class User < ApplicationRecord
+  VALID_EMALL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
   validates :name, presence: true, length: { maximum: 15 }
   
-  VALID_EMALL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z/i
-  validates :email, presence: true,
-  format: {with: VALID_EMALL_REGEX}
+  validates :email, presence: true, format: {with: VALID_EMALL_REGEX}
 
-  has_secure_password
- 
- VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
-　validates :password, presence: true,
-　format: { with: VALID_PASSWORD_REGEX }
+  has_secure_passwor
+　validates :password, presence: true,format: { with: VALID_PASSWORD_REGEX }
 end
