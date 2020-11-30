a835060cc7451cbf72b05e4cb428ba6e48a6ec3d
EC2 Default User <nisi_kazu_of_world@yahoo.co.jp>
2020-11-30 12:53:48 +0000

    06課題提出

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    #binding.pry
-    @user = User.new(name: params[:user][:name], email: params[:user][:email])
+    @user = User.new(user_params)
+    binding.pry
     if @user.save
       redirect_to root_path, success: '登録が完了しました'
     else
@@ -16,6 +16,6 @@ class UsersController < ApplicationController
   
   private
   def user_params
-    params.requuire(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   VALID_EMALL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z/i
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
-  validates :name, presence: true, length: { maximum: 15 }
   
-  validates :email, presence: true, format: {with: VALID_EMALL_REGEX}
-
-  has_secure_passwor
-　validates :password, presence: true,format: { with: VALID_PASSWORD_REGEX }
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, format: { with: VALID_EMALL_REGEX }
+  validates :password, presence: true,format: { with: VALID_PASSWORD_REGEX }
+  
+  has_secure_password
+  
+  has_many :topics
 end
